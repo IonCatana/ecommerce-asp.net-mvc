7764f2f5421deca6f421ec51f052d6e6912943fd
Ion Catana <catana.ion17@yahoo.it>
2022-09-08 14:47:35 +0200

    add delete action actors

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -51,7 +51,7 @@ namespace eTickets.Controllers
             return View(actorDetails);
         }
 
-        //Get: Actors/Create
+        //Get: Actors/Edit
         public async Task<IActionResult> Edit(int id)
         {
             var actorDetails = await _service.GetByIdAsync(id);
@@ -68,6 +68,23 @@ namespace eTickets.Controllers
                 return RedirectToAction(nameof(Index));
             }            
             return View(actor);
+        }
+        //Get: Actors/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
