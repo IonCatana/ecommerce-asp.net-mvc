2dab12fdea5b0b24e7b8f96d2576ce6527b8f47e
Ion Catana <catana.ion17@yahoo.it>
2022-09-09 12:33:12 +0200

    add update producer

--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -50,5 +50,24 @@ namespace eTickets.Controllers
             return View(producer);
         }
 
+        //GET: producers/edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Producer producer)
+        {
+            if (!ModelState.IsValid)           
+            {
+                await _service.UpdateAsync(id, producer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
+        }
+
     }
 }
