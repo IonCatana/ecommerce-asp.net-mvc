8e516cffe919662176128be77a4ef7b5f0236434
Ion Catana <catana.ion17@yahoo.it>
2022-09-18 14:49:46 +0200

    updated orders to include roles

--- eTickets/Models/Order.cs
+++ eTickets/Models/Order.cs
@@ -1,4 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace eTickets.Models
 {
@@ -6,9 +11,13 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Email { get; set; }
-        public string UserId { get; set; }
-        public List<OrderItem> OrderItems { get; set; }
 
+        public string Email { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
     }
 }
